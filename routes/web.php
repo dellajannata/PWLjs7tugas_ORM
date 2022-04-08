fdb4454c1421675fe0413b4db6f180c70a93adee
= <dellajannata2112@gmail.com>
2022-04-08 21:46:23 +0700

    Latihan Praktikum 9: Membuat relasi many to many

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,4 @@ Route::get('/barang', [BarangController::class, 'barang1']);
 Route::get('/pelanggan', [PelangganController::class, 'pelanggan1']);
 Route::resource('/mahasiswa', MahasiswaController::class);
 Route::get('/search', [MahasiswaController::class, 'search1'])->name('search');
+Route::get('/mahasiswa/nilai/{mahasiswa}', [MahasiswaController::class, 'mahasiswa_matakuliah1'])->name('nilai');
